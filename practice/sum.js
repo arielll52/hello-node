af0e7b24037cb6f4e47826acfe67d54a0c3bd32c
Ariel Liu <regina31664@gmail.com>
2021-10-18 12:40:04 +0800

    practice

---
+++ practice/sum.js
@@ -0,0 +1,17 @@
+console.log("hello world!");
+
+
+function sum(param) {
+    let sum = 0;
+ for (let i = 0; i <= param; i++){
+     sum = sum + i; 
+ }
+console.log(sum)
+}
+
+sum(3)
+sum(6)
+sum(10)
+// console.log(sum(3)); // 6
+// console.log(sum(6)); // 21
+// console.log(sum(10)); // 55
